7caa145b6d537772b2a3b34e0f67ff624da84cf0
Ompradeep <ompradeep2004@gmail.com>
2025-02-19 23:19:15 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
             <div className="flex-shrink-0">
               <div className="flex items-center gap-2">
                 <img 
-                  src="/VelSecure_logo.png" 
+                  src="https://github.com/ompradeep123/VelSecure/blob/main/VelSecure_logo.png" 
                   alt="VelSecure Logo" 
                   className="h-40 w-auto" // Changed from h-16 to h-48
                 />
